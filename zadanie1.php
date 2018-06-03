80f91983af705836e00075cf8751f671a2e67a6f
sayat23 <sayat23@mail.ru>
2018-06-03 19:37:05 +0600

    Dom zadanie2 and Dom zadanie3

--- zadanie1.php
+++ zadanie1.php
@@ -5,3 +5,4 @@ echo 'Меня зовут '.$name;
 echo '<br>';
 echo "Мне $age лет";
 echo '<br>';
+echo "“!|\/’”".'';
---
+++ zadanie2.php
@@ -0,0 +1,7 @@
+<?php
+const RISUNKI = 80;
+const FLOMASTER = 23;
+const KARANDASH = 40;
+
+$kraski = RISUNKI - FLOMASTER - KARANDASH;
+echo $kraski;
