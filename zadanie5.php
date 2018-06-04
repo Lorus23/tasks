f937cde4e73f2b96de76f8d48592767f27bc59eb
sayat23 <sayat23@mail.ru>
2018-06-04 11:36:11 +0600

    ыполняется 5е задание

---
+++ zadanie5.php
@@ -0,0 +1,9 @@
+<?php
+$bmw = ['model'=>'X5','speed'=>'120','doors'=>'5','year'=>'2015'];
+$toyota = ['model'=>'Camry','speed'=>'160','doors'=>'4','year'=>'2018'];
+$opel = ['model'=>'Omega','speed'=>'90','doors'=>'4','year'=>'2000'];
+
+
+$car = [$bmw, $toyota, $opel];
+echo '<pre>';
+var_dump($car);
