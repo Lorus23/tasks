7e67dfb4f366cd98ba6739b30c845b900925b557
sayat23 <sayat23@mail.ru>
2018-06-04 12:33:40 +0600

    доработано задание 6

--- zadanie6.php
+++ zadanie6.php
@@ -1,28 +1,17 @@
+<h1>Таблица умножения</h1>
 <?php
-$cols = 10;  // ячейка
-$rows = 10;  // строка
-?>
-
-<html>
-    <title>Таблица умножения</title>
-<body>
-    <!-- Заголовок -->
-    <h1>Таблица умножения</h1>
-    <!-- Таблица -->
-    <?php
-    echo "<table border = '1' width='400'>";
-    for ($tr = 1; $tr <= $rows; $tr++) { // минимальная таблица умножения - это 1 х 1
-        echo "<tr>"; // рисует строку (внутри <tr> находится <td> - нужен очередной цикл)
-        for ($td = 1; $td <= $cols; $td++) {
-           if ($tr>1 and $td>1 and $tr % 2 == 0 and $td % 2 == 0) {echo "<td>" .'('. $tr * $td .')'. "</td>"; }
-           elseif ($tr>1 and $td>1 and $tr % 2 != 0 and $td % 2 != 0) {echo "<td>" .'['. $tr * $td .']'. "</td>"; }
-           else
-               {echo "<td>" . $tr * $td . "</td>";} // $tr * $td - для вывода значений (цифр) внутри таблицы
-
+echo '<table border="1">';
+for ($i = 1; $i <= 10; $i++) {
+    echo '<tr>';
+    for ($j = 1; $j <= 10; $j++)
+        if ($i > 1 and $j > 1 and $i % 2 == 0 and $j % 2 == 0) {
+            echo "<td>" . '(' . $i * $j . ')' . "</td>";
+        } elseif ($i > 1 and $j > 1 and $i % 2 != 0 and $j % 2 != 0) {
+            echo "<td>" . '[' . $i * $j . ']' . "</td>";
+        } else {
+            echo "<td>" . $i * $j . "</td>";
         }
-        echo "</tr>";
-    }
-    echo "</table>";
-    ?>
-</body>
-</html>
+}
+echo '</tr>';
+echo '</table>';
+?>
